738d2d2149ced43afe1f80647c3a73a27c471904
Bartok <danielrpike9@gmail.com>
2026-07-17 15:39:35 -0400

    docs(run): fix GROUND_ID comment to say ground container (#109)
    Copy-paste error: the GROUND_ID default comment still said "simulation
    container" (same wording as SIM_ID). Clarify it is the ground container
    last octet used for ROS_DOMAIN_ID / docker IP placement.

--- tools_and_docs/deploy_run.sh
+++ tools_and_docs/deploy_run.sh
@@ -21,7 +21,7 @@ ODOM="${ODOM:-none}" # Options: none (default), openvins, fastlio, superodom, mi
 SIM_SUBNET="${SIM_SUBNET:-10.42}" # Simulation subnet (default = 10.42)
 AIR_SUBNET="${AIR_SUBNET:-10.22}" # Inter-vehicle subnet (default = 10.22)
 SIM_ID="${SIM_ID:-100}" # Last byte of the simulation container IP (default = 100)
-GROUND_ID="${GROUND_ID:-101}" # Last byte of the simulation container IP (default = 101)
+GROUND_ID="${GROUND_ID:-101}" # Last byte of the ground container IP (default = 101)
 #
 DRONE_TYPE="${DRONE_TYPE:-quad}" # Options: quad (default), vtol, tail
 DRONE_ID="${DRONE_ID:-1}" # Id of aircraft (default = 1)
